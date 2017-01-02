bb8ec636610e1775e7db71cb6eff538c1fcccf82
이상민 <@DESKTOP-C5I412K>
2017-01-02 15:43:48 +0900

    1st

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+import java.util.Scanner;
+
+/*Hello World 예 제*/
+public class HelloWorld {
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);	
+	System.out.println("Hello WOrld");
+	String temp = s.nextLine();
+	System.out.println(temp);
+	}
+}
